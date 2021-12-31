9cbd8a46c3ba73e1c4d7ec0fb5e19bd782635159
tubi55 <tubi55@nate.com>
2021-12-31 09:43:10 +0900

    썸네일 클릭시 팝성 생성 기능 추가

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -3,6 +3,9 @@ import {useEffect, useState} from "react";
 
 function Gallery(){
   let [items, setItems] = useState([]);
+  //팝업생성 관리할 state생성
+  let [isPop, setIsPop] = useState(false);
+
   const api_key = "e7ed3b39fe112d7e93d03c19325305e0";
   const url = `https://www.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=${api_key}&per_page=10&format=json&nojsoncallback=1`;
 
@@ -16,11 +19,11 @@ function Gallery(){
   },[]);
 
   return (
-    <main>
+    <main className="gallery">
       <div className="inner">
         <h1><a href="#">Gallery</a></h1>
 
-        <section>
+        <section className="">
           {            
             items.map((item,index)=>{
               const imgSrc = `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`;
@@ -28,7 +31,9 @@ function Gallery(){
               return (
                 <article key={index}>
                   <div className="inner">
-                    <div className="pic">
+                    <div className="pic" onClick={()=>{
+                      setIsPop(true);
+                    }}>
                       <img src={imgSrc} />
                     </div>
 
@@ -41,8 +46,22 @@ function Gallery(){
         </section>
         
       </div>
-    </main>
+      {/* isPop 상태값이 true면 팝업 보임 */}
+      { isPop ? <Pop /> : null }
+      
+    </main>    
   )
+
+  //팝업생성하는 컴포넌트 함수 정의
+  function Pop(){
+    return (
+      <aside className="pop">
+        <span onClick={()=>{
+          setIsPop(false);
+        }}>Close</span>
+      </aside>
+    )
+  }
 }
 
 export default Gallery;
