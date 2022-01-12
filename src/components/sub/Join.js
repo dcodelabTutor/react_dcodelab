a370f99e755eb39579033dd1d238a1f4f3e8f920
tubi55 <tubi55@nate.com>
2022-01-12 11:24:31 +0900

    userid에 해당하는 value값의 인증처리 완료후 useEffect에서 에러결과확인

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -1,47 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Join(){
-  //state로 관리할 초기 value값들
   const initVal={
     userid : ''   
   }
-  //useState로 초기 value값을 state에 담아서 관리 시작
   const [val, setVal] = useState(initVal);
   const [err, setErr] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
 
-  //input에 변화점이 생길때마다 실행될 함수
   const handleChange = e => {   
-    const {name, value} = e.target;  
-    //현재 비어있는 초기 객체값을 내가 현재 입력하고 있는 새로운 value값으로 계속 덮어쓰기
-    setVal({...val, [name]: value});  
-    console.log(val);  
+    const {name, value} = e.target;     
+    setVal({...val, [name]: value}); 
   }
 
   //submit이벤트 발생하면 실행되는 함수
-  const handleSubmit = e =>{
-    //일단 기본 전송을 막음
-    e.preventDefault();
-    //setErr로  기존의 err값을 변경
-    //변경할 err객체내용을 반환해주는 check함수 호출
-    setErr(check(val));   
-    console.log(err); 
+  const handleSubmit = e =>{ 
+    e.preventDefault();   
+    setErr(check(val));  
   }
 
   //에러 객체를 반환하는 함수
   const check = val=>{
-    let errs = {};
-    const eng = /[a-zA-Z]/;
-    const num = /[0-9]/
-    const spc = /[!@#$%^&*]/;
-
-    //현재 스테이트 val의 userid값이 비어있거나 5글자 미만일때만
-    if( !val.userid || val.userid.length <5 ){
-      //비어 있는 err객체에 userid키값을 만들어서 에러문구를 담음
-      errs.userid = '아이디를 5글자 이상 입력하세요';
-    }    
+    let errs = {};  
+    if( !val.userid || val.userid.length <5 ) errs.userid='아이디 5글자 이상입력';
     return errs;
   }
+
+  //전송 버튼을 눌러서 err state값이 바뀔때에만 호출
+  useEffect(()=>{
+    //해당 코드 블록안에서 err스테이트에 담겨있는 객체값이 비어있으면
+    //모든 인증을 통과한 상태라서 회원가입 완료처리
+    console.log(err);
+  },[err]);
 
   return (
     <main className='join'>
