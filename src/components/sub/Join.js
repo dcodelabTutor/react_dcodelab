d438ad5c3021cdcc792736b2d38cdf83ca4cacce
tubi55 <tubi55@nate.com>
2022-01-12 10:51:04 +0900

    전송버튼을 클릭한 순간 val의 값이 조건에 부합되지 않으면 에러문구가 담긴 객체 생성

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -3,17 +3,44 @@ import { useState } from "react";
 function Join(){
   //state로 관리할 초기 value값들
   const initVal={
-    userid : ''
+    userid : ''   
   }
   //useState로 초기 value값을 state에 담아서 관리 시작
   const [val, setVal] = useState(initVal);
+  const [err, setErr] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
   //input에 변화점이 생길때마다 실행될 함수
   const handleChange = e => {   
-    const {name, value} = e.target;
-    //console.log(`name:${name}, value:${value}`);
-    setVal({...val, [name]: value});
-    console.log(val);
+    const {name, value} = e.target;  
+    //현재 비어있는 초기 객체값을 내가 현재 입력하고 있는 새로운 value값으로 계속 덮어쓰기
+    setVal({...val, [name]: value});  
+    console.log(val);  
+  }
+
+  //submit이벤트 발생하면 실행되는 함수
+  const handleSubmit = e =>{
+    //일단 기본 전송을 막음
+    e.preventDefault();
+    //setErr로  기존의 err값을 변경
+    //변경할 err객체내용을 반환해주는 check함수 호출
+    setErr(check(err));   
+    console.log(err); 
+  }
+
+  //에러 객체를 반환하는 함수
+  const check = val=>{
+    let errs = {};
+    const eng = /[a-zA-Z]/;
+    const num = /[0-9]/
+    const spc = /[!@#$%^&*]/;
+
+    //현재 스테이트 val의 userid값이 비어있거나 5글자 미만일때만
+    if( !val.userid || val.userid.length <5 ){
+      //비어 있는 err객체에 userid키값을 만들어서 에러문구를 담음
+      errs.userid = '아이디를 5글자 이상 입력하세요';
+    }    
+    return errs;
   }
 
   return (
@@ -21,7 +48,7 @@ function Join(){
       <div className="inner">
         <h1><a href="#">Join</a></h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <fieldset>
             <legend className='h'>회원가입 입력 폼 양식</legend>
 
