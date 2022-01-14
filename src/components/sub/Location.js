f1961087a13e0daa97ca7f2487a1a2e93828a32a
tubi55 <tubi55@nate.com>
2022-01-14 09:41:28 +0900

    카카오맵 api연결 및 인스턴스 생성해서 지도출력

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,9 +1,29 @@
-function Location(){
-  return (
-    <main>
-      <div className="inner">
-        <h1><a href="#">Location</a></h1>
+import { useEffect, useRef } from "react";
 
+function Location(){
+  //윈도우 전역에 등록되어 있는 kakao객체를 불러옴
+  const {kakao} = window;
+  //useRef로 #map 참조
+  const container = useRef(null);
+
+  //컴포넌트 생성시
+  useEffect(()=>{
+    const options = { 
+      center: new kakao.maps.LatLng(37.5132313,127.0594368), 
+      level: 3 
+    }
+
+    //카카오맵 생성자로부터 인스턴스 복사해서 맵 실행
+    const map = new kakao.maps.Map(container.current, options);
+  },[]);
+
+  return (
+    <main className='location'>
+      <div className="inner">
+        <h1>Location</h1>
+
+        {/* 맵이 출력될 프레임 useRef로 참조 */}
+        <div id="map" ref={container}></div>
       </div>
     </main>
   )
