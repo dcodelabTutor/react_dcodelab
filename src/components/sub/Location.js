c351773247445811c0684f0fb388df621fc25fa4
tubi55 <tubi55@nate.com>
2022-01-14 09:50:29 +0900

    트래픽보기 기능 호출

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,10 +1,12 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function Location(){
   //윈도우 전역에 등록되어 있는 kakao객체를 불러옴
   const {kakao} = window;
   //useRef로 #map 참조
   const container = useRef(null);
+  //생성된 map인스턴스가 담길 state생성
+  const [map, setMap] = useState(null);
 
   //컴포넌트 생성시
   useEffect(()=>{
@@ -15,6 +17,8 @@ function Location(){
 
     //카카오맵 생성자로부터 인스턴스 복사해서 맵 실행
     const map = new kakao.maps.Map(container.current, options);
+
+    setMap(map);
   },[]);
 
   return (
@@ -24,6 +28,17 @@ function Location(){
 
         {/* 맵이 출력될 프레임 useRef로 참조 */}
         <div id="map" ref={container}></div>
+
+        <ul className="traffic">
+          <li onClick={()=>{
+            //버튼 클릭시 state map에 담겨있는 인스턴스로부터 기능호출
+            map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+          }}>교통정보 보기</li>
+
+          <li onClick={()=>{
+            map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);  
+          }}>교통정보 끄기</li>
+        </ul>
       </div>
     </main>
   )
