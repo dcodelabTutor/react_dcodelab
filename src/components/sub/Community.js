af33ce4fa7f210238837d8ac22d069bd23543cf6
tubi55 <tubi55@nate.com>
2022-01-18 11:40:01 +0900

    데이터 삭제

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -6,18 +6,19 @@ function Community() {
   const [post, setPost] = useState('');
   const [postList, setPostlist] = useState([]);
 
-  //기존 포소트목록에서 새로운 포스트를 추가하는 함수
   const insertPost = ()=>{
     setPostlist([...postList, post])     
   }
 
-  //글 입력해서 컴포넌트 재 랜더링시
+  const deletePost = delIndex =>{
+    setPostlist(
+      postList.filter((post, index)=> (index !== delIndex))
+    )
+  }
+
   useEffect(()=>{
-    frame.current.classList.add('on');
-    //기존 input요소 비우고
-    setPost('');
-    //추가된 리스트 목록 콘솔 출력
-    console.log(postList);
+    frame.current.classList.add('on');   
+    setPost('');  
   },[postList])
 
   return (
@@ -29,11 +30,9 @@ function Community() {
           <input 
             ref={input}
             type="text" 
-            value= {post}
-            //changeEvent가 발생할때마다 post값  변경
+            value= {post}    
             onChange={e=> setPost(e.target.value)}
-          />
-          {/* 저장버튼 클릭시 insertPost함수 호출해서 글저장 */}
+          />   
           <button
             onClick ={insertPost}
           >save
@@ -42,13 +41,16 @@ function Community() {
 
         <section className="showList">
           {
+            //기존 배열값을 역순으로 화면에 출력
             postList.map((post, index)=>{
               return (
                 <article
                   key={index}
                 >
                   <p>{post}</p>
-                  <span>del</span>
+                  <span 
+                    onClick={()=> deletePost(index)}
+                  >del</span>
                 </article>
               )
             })
