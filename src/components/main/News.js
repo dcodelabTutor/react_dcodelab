31130f97bf2b1c992f601993e6c4cf399513762d
tubi55 <tubi55@nate.com>
2022-01-20 11:42:03 +0900

    localStorage의 데이터를 메인페이지에 출력

--- src/components/main/News.js
+++ src/components/main/News.js
@@ -1,6 +1,37 @@
 function News(){
+  
+  const getLocalItems=()=>{
+    let data = localStorage.getItem('posts');
+    
+    if(data){
+      return JSON.parse(data);
+    }else{
+      return [];
+    }
+  }
+
+  const posts = getLocalItems();
+  console.log(posts);
+        
   return (
-    <section id='news' className='myScroll'></section>
+    <section id='news' className='myScroll'>
+      <div className="inner">
+        <h2>RECENT NEWS</h2>
+        
+        <div className="txtBox">
+          {
+            posts.map((post,index)=>{
+              return (
+                <article key={index}>
+                  <h3>{post.title}</h3>
+                  <p>{post.content}</p>
+                </article>
+              )
+            })
+          }
+        </div>
+      </div>
+    </section>
   )
 }
 
