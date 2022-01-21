f83982376cca2790085b275133466ad50072a7c7
tubi55 <tubi55@nate.com>
2022-01-21 11:26:22 +0900

    메인컴포넌트에서 store의 reducer데이터 출력

--- src/components/main/Intro.js
+++ src/components/main/Intro.js
@@ -1,6 +1,25 @@
-function Intro(){
+import { useSelector } from "react-redux";
+
+function Intro(){  
+  const members = useSelector(state=>state.memberReducer.members);
+  console.log(members);
+
   return (
-    <section id='intro' className='myScroll'></section>
+    <section id='intro' className='myScroll'>
+      <div className="inner">
+        <h2>Introduce Members</h2>
+        {
+          members.map((member, index)=>{
+            return (
+              <article key={index}>
+                <h3>{member.name}</h3>
+                <p>{member.position}</p>
+              </article>
+            )            
+          })
+        }
+      </div>
+    </section>
   )
 }
 
